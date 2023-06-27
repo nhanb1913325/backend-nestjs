0c3fb8ecd7106cc8baab9f0375a723891934fa41
nhanb1913325 <nhanb1913325@student.ctu.edu.vn>
2023-06-27 20:13:46 +0700

    Doc file md Linked List

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { CryptographyController } from './algorithms/cryptography/cryptography.controller';
 import { GraphController } from './algorithms/graph/graph.controller';
 import { ImageProcessingController } from './algorithms/image-processing/image-processing.controller';
+import { LinkedListController } from './algorithms/linked-list/linked-list.controller';
 
 @Module({
   controllers: [
     CryptographyController,
     GraphController,
     ImageProcessingController,
+    LinkedListController,
   ],
 })
 export class AppModule {}
