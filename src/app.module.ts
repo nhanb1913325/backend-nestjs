a458db62f4b10d514963efb88670635be71762cf
nhanb1913325 <nhanb1913325@student.ctu.edu.vn>
2023-06-27 19:33:43 +0700

    Doc file md Image Processing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { CryptographyController } from './algorithms/cryptography/cryptography.controller';
 import { GraphController } from './algorithms/graph/graph.controller';
+import { ImageProcessingController } from './algorithms/image-processing/image-processing.controller';
 
 @Module({
-  controllers: [CryptographyController, GraphController],
+  controllers: [
+    CryptographyController,
+    GraphController,
+    ImageProcessingController,
+  ],
 })
 export class AppModule {}
