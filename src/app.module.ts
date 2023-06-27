430f5072b4400d31061e2be0ad956f09059df49b
nhanb1913325 <nhanb1913325@student.ctu.edu.vn>
2023-06-27 19:20:57 +0700

    Doc file md Cryptography

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { CryptographyController } from './algorithms/cryptography/cryptography.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [CryptographyController],
 })
 export class AppModule {}
